2e3de694a01072b4eb0e8a458a32b80926062e8c
Jacques Ehrardt <ehrardt38@gmail.com>
2022-11-03 21:27:43 -0300

    First Commit

---
+++ script.js
@@ -0,0 +1,147 @@
+const options = {
+    chart: {
+        type: 'area',
+        stacked: false,
+        height: 350,
+        zoom: {
+          type: 'x',
+          enabled: true,
+          autoScaleYaxis: true
+        },
+        foreColor: '#fff'
+    },
+    dataLabels: {
+      enabled: false
+    },
+    markers: {
+      size: 5,
+    },
+    fill: {
+        type: 'gradient',
+        gradient: {
+            shadeIntensity: 1,
+            inverseColors: false,
+            opacityFrom: 0.45,
+            opacityTo: 0.05,
+            stops: [20, 100, 100, 100]
+          },
+      },
+    xaxis: {
+      type: "datetime",
+      labels: {
+        formatter: function(val, timestamp) {
+          return moment(timestamp).format('DD MMMM HH:mm');
+        },
+        offsetX: 20,
+      },
+      axisBorder: {
+        show: false
+      },
+      axisTicks: {
+        show: false
+      }
+    },
+    yaxis: {
+        labels: {
+          offsetX: -10,
+          offsetY: -5
+        },
+        tooltip: {
+          enabled: true
+        }
+    },
+    grid: {
+      padding: {
+        left: -5,
+        right: 5
+      }
+    },
+    title: {
+        align: 'left',
+        offsetX: 14
+    },
+    tooltip: {
+      x: {
+        format: "dd MMM yyyy HH:mm"
+      },
+    },
+    legend: {
+      position: 'top',
+      horizontalAlign: 'left'
+    },
+  };
+
+fetch("https://apigato.herokuapp.com/")
+.then((response) => {
+  return response.json()
+}).then((json) => {
+  const optionsChartTagBranca = {
+    ...options,
+    title: {
+      text: 'Consumo de Água da Khaleesi'
+    },
+    colors:['#FF7750'],
+    series: [json[0]]
+  }
+  const optionsChartTagVerde = {
+    ...options,
+    title: {
+      text: 'Consumo de Água da Lisa'
+    },
+    colors:['#42BAFE'],
+    series: [json[1]]
+  }
+  const chartTagBranca = new ApexCharts(document.querySelector("#chartTagBranca"), optionsChartTagBranca);
+  chartTagBranca.render();
+
+  const chartTagVerde = new ApexCharts(document.querySelector("#chartTagVerde"), optionsChartTagVerde);
+  chartTagVerde.render();
+})
+  
+
+const optionsBar = {
+  series: [{
+    name: 'Khaleesi',
+    data: [32.18, 31.73, 31.66, 0]
+  }, {
+    name: 'Lisa',
+    data: [62.31, 16.86, 2.74, 2.89]
+  }],
+  chart: {
+    type: 'bar',
+    height: 300,
+    foreColor: '#fff'
+  },
+  title: {
+    text: 'Consumo de Água Total por Dia'
+  },
+  colors:['#FF7750', '#42BAFE'],
+  plotOptions: {
+    bar: {
+      horizontal: false,
+      columnWidth: '55%',
+      endingShape: 'rounded'
+    },
+  },
+  dataLabels: {
+    enabled: false
+  },
+  stroke: {
+    show: true,
+    width: 2,
+    colors: ['transparent']
+  },
+  xaxis: {
+    categories: ['31/10', '01/11', '02/11', '03/11'],
+  },
+  yaxis: {
+
+  },
+  fill: {
+    opacity: 1
+  },
+  tooltip: {}
+};
+
+const chartBar = new ApexCharts(document.querySelector("#chartBar"), optionsBar);
+chartBar.render();
